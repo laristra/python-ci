2048a9afa504d49083b0cad65b62bbc41cdf8e1e
Laurel McIntyre <lmcintyre@lanl.gov>
2017-06-14 14:11:32 -0600

    added code coverage
    using unittests framework and adding excluded code lines

--- mytan.py
+++ mytan.py
@@ -4,8 +4,13 @@ def mytan(x):
     y = math.sin(x)/math.cos(x)
     return(y)
 
-
+result = mytan(4)
 assert(round(mytan(3),5)==round(-0.142546543074,5))
 assert(round(mytan(3.14),5)==round(-0.0015926549364,5))
 assert(round(mytan(2.5),5)==round(math.tan(2.5),5))
 
+if(True==False):
+ a=1
+ b=2
+ c=a*b
+ print "c"
