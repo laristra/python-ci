24c3c3d6437d4f7a82bea2d6ac474380d90f1867
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-14 14:55:29 -0600

    Create mytan.py
    add factorial

--- mytan.py
+++ mytan.py
@@ -9,8 +9,9 @@ assert(round(mytan(3),5)==round(-0.142546543074,5))
 assert(round(mytan(3.14),5)==round(-0.0015926549364,5))
 assert(round(mytan(2.5),5)==round(math.tan(2.5),5))
 
-if(True==False):
- a=1
- b=2
- c=a*b
- print "c"
+def factorial(x):
+    if x > 0:
+        y = math.factorial(x)
+        return(y)
+result = factorial(7)
+print result
