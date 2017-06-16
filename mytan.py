6095c0e27a7f9eac8ceafa5ec6986370f8fedcbe
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-16 11:35:30 -0600

    delete print

--- mytan.py
+++ mytan.py
@@ -6,7 +6,6 @@ def mytan(x):
     return y
 
 def test():
-    print "mytan tests"
     assert(round(mytan(3),5)==round(-0.142546543074,5))
     assert(round(mytan(3.14),5)==round(-0.0015926549364,5))
     assert(round(mytan(2.5),5)==round(math.tan(2.5),5))
