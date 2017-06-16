002604d39bffd5ee339743e8fa1c59d285e23849
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-16 12:51:27 -0600

    change function name

--- mytan.py
+++ mytan.py
@@ -5,7 +5,7 @@ def mytan(x):
     y = math.sin(x)/math.cos(x)
     return y
 
-def test():
+def test_mytan():
     assert(round(mytan(3),5)==round(-0.142546543074,5))
     assert(round(mytan(3.14),5)==round(-0.0015926549364,5))
 
