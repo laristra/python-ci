e4af2244972ac4fb23e7661f2f0bf1f0a0ced89e
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-19 11:19:22 -0600

    move tests to other file

--- mytan.py
+++ mytan.py
@@ -5,10 +5,6 @@ def mytan(x):
     y = math.sin(x)/math.cos(x)
     return y
 
-def test_mytan():
-    assert(round(mytan(3),5)==round(-0.142546543074,5))
-    assert(round(mytan(3.14),5)==round(-0.0015926549364,5))
-
 if __name__ == "__main__":
     arg=float(sys.argv[1])
     print("Tan of %s is %s" % (arg, mytan(arg)))
