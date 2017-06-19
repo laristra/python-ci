0c9370082066bc2ebdde61c5b0351c6480a84ff4
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-19 11:28:49 -0600

    delete tests

--- myfactorial.py
+++ myfactorial.py
@@ -4,8 +4,3 @@ def factorial(x):
     if x > 0:
         y = math.factorial(x)
         return(y)
-
-def test_myfactorial():
-    assert(factorial(4))==24
-    assert(factorial(7))==5040
-    assert(factorial(1))==1
