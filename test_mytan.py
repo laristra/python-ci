8a93905ba7364b2ea90702dff3497e7bab21c336
laurelmcintyre <lmcintyre@lanl.gov>
2017-06-20 10:23:54 -0600

    delete add

--- test_mytan.py
+++ test_mytan.py
@@ -1,4 +1,4 @@
-from mytan import mytan, add
+from mytan import mytan
 def test_mytan():
     assert(round(mytan(3),5)==round(-0.142546543074,5))
     assert(round(mytan(3.14),5)==round(-0.0015926549364,5)) 
